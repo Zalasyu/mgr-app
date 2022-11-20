5d7e8de953c36293126709511980e0f7f3d76d55
Zalasyu <alec_moldovan@yahoo.com>
2022-11-20 11:20:25 -0600

    requirements wasnt pushed??

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,6 +1,6 @@
 import requests
 
-resp = requests.post('http://localhost:5000/predict',
+resp = requests.post('https://mgr-osu.herokuapp.com/predict',
                      files={'file': open('blues.00000.wav', 'rb')})
 
 print(resp.text)
